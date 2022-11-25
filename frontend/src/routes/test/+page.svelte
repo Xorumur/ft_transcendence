43da5e05b502548bd6edf5c8547ac5637b7263ff
Mathieu <mlecherb@c4r2p4.42nice.fr>
2022-11-25 05:01:04 +0100

    Doc about back & front && Back and front can now communicate

---
+++ frontend/src/routes/test/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+		import { onMount } from 'svelte';
+		import axios from 'axios';
+		let t: any;
+		onMount(async () => {
+			console.log('mounted');
+			t = await axios.get("http://localhost:4200/test");
+			t = t.data;
+			console.log(t);
+		});
+</script>
+
+{#if t}
+	<div>
+		Salut a tous.
+		<p>{t}</p>
+	</div>
+{/if}
